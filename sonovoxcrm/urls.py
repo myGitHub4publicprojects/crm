994ae16c61a2abe1bb7bf3719b153f4ae6107cf0
myGitHub4publicprojects <jszczep@gmail.com>
2018-01-19 15:52:51 +0100

    update urls

--- sonovoxcrm/urls.py
+++ sonovoxcrm/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^crm/', include('crm.urls')),
+	url(r'^', include('crm.urls')),
     url(r'^admin/', admin.site.urls),
 ]
