297b07931a7e7520dfb932716790710f45d29ede
myGitHub4publicprojects <jszczep@gmail.com>
2020-08-19 14:02:27 +0200

    remove usunsed code

--- crm/models.py
+++ crm/models.py
@@ -40,7 +40,7 @@ class SZOI_Errors(models.Model):
 	line = models.TextField()
 
 class Patient(models.Model):
-    	first_name = models.CharField(max_length=120)
+	first_name = models.CharField(max_length=120)
 	last_name = models.CharField(max_length=120)
 	date_of_birth = models.DateField(
 		auto_now=False, auto_now_add=False, null=True, blank=True)
@@ -101,22 +101,7 @@ class PCPR_Estimate(Finance):
 
 class Invoice(Finance):
     pass
-	# payed = models.BooleanField(default=False, verbose_name='zapłacona')
-	# # change this to True once payment received and HA dispenced
-	# type = models.CharField(max_length=8, choices=(
-	# 	('transfer', 'transfer'), ('cash', 'cash')), verbose_name='rodzaj płatności')
-	# date = models.DateField(default=datetime.date.today())
 
-	# def get_number(self):
-	# 	return str(self.pk) + self.timestamp.strftime("/%m/%d")
-	# def get_absolute_url(self):
-	# 	return reverse('crm:invoice_update', kwargs={'pk': self.pk})
-
-# class Corrective_Invoice(Finance):
-# 	invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
-# 	date = models.DateField(default=datetime.date.today())
-# 	def get_number(self):
-# 		return str(self.pk) + self.timestamp.strftime("/%m/%d")
 
 
 class Device(models.Model):
